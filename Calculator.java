d1b26760bb6005c04d8ec2a82e088c22ebb5d4bc
Sara Pojaku <115804147+sarapojaku@users.noreply.github.com>
2025-08-31 15:13:21 +0200

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    protected double num1;
+    protected double num2;
+
+    public Calculator(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+        public double calculate() {
+        return 0;
+    }
+}
